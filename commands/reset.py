4bff1147dd98ed7f4f1e45cf1aac69f65b6748c3
jobayer1n1 <jobayerahmed1n1@gmail.com>
2026-03-14 22:29:06 +0600

    bugfix

--- commands/reset.py
+++ commands/reset.py
@@ -1,5 +1,5 @@
 from utils.localAppData import LocalAppData
-
+HELP = "Reset all configs"
 def main(argv: list[str]) -> None:
     user_input = input("Reset won't delete any files from the syncing directory.\nDo you really want to reset all configs? (y/n)")
     if user_input.lower() == "y":
